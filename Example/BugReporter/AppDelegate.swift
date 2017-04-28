4171f04c9211d4819d2cf82191313b2f43f904f1
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2017-04-27 21:12:38 -0300

    Updated example

--- Example/BugReporter/AppDelegate.swift
+++ Example/BugReporter/AppDelegate.swift
@@ -14,14 +14,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate, BugReporterDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         _ = BugReporter.setup(with: launchOptions, delegate: self)    // call as soon as possible
                        .report(using: .email)   // can set it or change it later
+                       .debug(false)  // enable or disable debug at any time
         
         return true
     }
 
+    
+    //MARK: Bug Report delegate
+    func userDidStartAReport(_ report: Report) {
+        debugPrint("userDidStartAReport: \(report)")
+    }
+    
+    func userDidCancelAReport(_ report: Report) {
+        debugPrint("userDidCancelAReport: \(report)")
+    }
+    
+    func userDidSendAReport(_ report : Report, using mode: ReportingMode) {
+        debugPrint("userDidSendAReport: \(report) using: \(mode.rawValue)")
+    }
+    
+    func userDidSaveAReport(_ report: Report, using mode: ReportingMode) {
+        debugPrint("userDidSaveAReport: \(report) using: \(mode.rawValue)")
+    }
+    
+    func appendMoreImages(to report: Report) -> [UIImage]? {
+        return nil
+    }
+    
 }
 
